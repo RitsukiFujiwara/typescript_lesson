1e2175a4948788c67bbd59ee36dbb3ed95a0535c
Fujiwara <r9.20important@icloud.com>
2021-05-03 20:52:24 +0900

    完了

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,104 @@ let nameObj : NAME = {
 const func1 = (x:number,y:number) => {
   return x + y;
 }
+// IntersectionTypes
+type PROFILE = {
+  age: number;
+  city: string;
+};
 
-function App() {
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 21,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
+};
+//Union Types
+let value: boolean | number;
+value = true;
+value = 2;
+
+let arrUni: (number | string)[];
+arrUni = [0,1,"test"];
+
+let company: "Facebook" | "Google" | "Amazon"
+company = "Amazon";
+
+//typeof
+let msg: string = "Hi";
+let msg2: typeof msg;
+msg2 = "h3llo";
+
+let animal = {cat: "small cat"};
+let newAnimal: typeof animal = {cat: "big cat"};
+
+// keyof
+type KEYS = {
+  primary: string;
+  secondary: string;
+}
+let key: keyof KEYS;
+key = "primary";
+
+// typeof + keyof
+const SPORTS = {
+  soccer: "SOCCER",
+  baseball: "Baseball",
+}
+let keySports: keyof typeof SPORTS;
+keySports = "soccer";
+
+// enum
+ enum OS {
+   Windows,
+   Mac,
+   Linux,
+ }
+
+ interface PC {
+   id: number,
+   OSType: OS;
+ }
+ const PC1:PC = {
+    id: 1,
+    OSType: OS.Windows,
+ }
+ const PC2:PC = {
+  id: 2,
+  OSType: OS.Mac,
+}
+// 型の互換性
+const comp1 = "test";
+let comp2:string = comp1;
+
+// Generics  ジェネリックス
+// 型を動的に変更出来る
+interface GEN<T>{
+  item: T;
+}
+const gen0: GEN<string> = { item: "hello"}
+const gen1: GEN<number> = { item: 2 };
+
+interface GEN1<T extends string | number>{
+  item: T;
+}
+const gen2: GEN<string> = { item: "hello" };
+const gen3: GEN<number> = { item: 2 };
+
+function funcGen<T>(props: T){
+  return {item: props}
+}
+const gen4 = funcGen("test");
+const gen5 = funcGen<string | null>(null);
+
+const App:React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
